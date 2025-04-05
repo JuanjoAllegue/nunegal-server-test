fa14e4c71605a15ed2f007e3d27f3584221e47a3
jallegue <juan.allegue.criado@udc.es>
2025-04-05 10:20:52 +0200

    Initial commit

---
+++ src/main/java/com/example/nunegal_server_test/NunegalServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nunegal_server_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunegalServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunegalServerTestApplication.class, args);
+	}
+
+}
